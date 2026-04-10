18de7ba386347c0ccf0414367c0907b15707cbe9
jamiu <aarejamiu1@gmail.com>
2026-04-10 06:44:15 +0100

    extensions

--- index.js
+++ index.js
@@ -2,80 +2,114 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const mongoose = require('mongoose');
+const UserModel = require('./models/user.model');
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:true}))
 
-app.use(express.static('images'))
-app.get('/products', (req, res) => {
-    const products = [
-        {
-            name: "Laptop",
-            price: 500000,
-            image: "/Laptop.jpg"
-        },
-        {
-            name: "Phone",
-            price: 200000,
-            image: "/Phone.jpg"
-        },
-        {
-            name: "Headphones",
-            price: 50000,
-            image: "/Hphone.jpg"
-        },
-        {
-            name: "Smart Watch",
-            price: 80000,
-            image: "/smartwatch.jpg"
-        },
-        {
-            name: "Bluetooth Speaker",
-            price: 40000,
-            image: "/speaker.webp"
-        },
-        {
-            name: "Gaming Mouse",
-            price: 25000,
-            image: "/mouse.jpg"
-        },
-        {
-            name: "Keyboard",
-            price: 30000,
-            image: "/keyboard.jpg"
-        },
-        {
-            name: "Tablet",
-            price: 180000,
-            image: "/tablet.jpg"
-        },
-        {
-            name: "Camera",
-            price: 350000,
-            image: "/camera.webp"
-        },
-        {
-            name: "Power Bank",
-            price: 15000,
-            image: "/power-bank.jpg"
-        }   
+// let gender = "Male";
+// app.get("/users",(req,res)=>{
+//     res.render("allUsers", { gender });
+// })
+
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() =>
+    {console.log("Connected to MongoDB")
+})
+.catch((error) =>{
+    console.log("Error connecting to MongoDB:", error)
+});
+
+const users = [
+        { fullname: "John Doe", age: 30, email: "john.doe@example.com", course: "Software Engineering" },
+        { fullname: "Jane Smith", age: 25, email: "jane.smith@example.com", course: "Data Science" },
+        { fullname: "Alice Johnson", age: 28, email: "alice.johnson@example.com", course: "Web Development" },
+        { fullname: "Bob Brown", age: 35, email: "bob.brown@example.com", course: "Mobile Development" }
     ];
 
-    let html = "<h1>My Products</h1>";
-
-    products.forEach(product => {
-        html += `
-            <div style="border:1px solid #ccc; padding:10px; margin:10px; width:200px;">
-            <img src="${product.image}" style="width:100%; height:150px; object-fit:cover;" />
-                <h3>${product.name}</h3>
-                <p>Price: ₦${product.price}</p>
-            </div>
-        `;
-    });
-
-    res.send(html);
+app.get('/users', (req, res) => {
+    res.render("allUsers", { users });
 });
-app.listen(process.env.PORT,(error)=>{
-    if(error){
-        console.log(`Error occurred while starting the server: ${error}`);
-    }else{
-        console.log(`Server is running`);
+
+app.get('/adduser', (req,res) => {
+    res.render('addUser')
+})
+app.post('/adduser', (req,res) =>{
+    console.log(req.body)
+
+    const { fullname, email, course, age } = req.body
+
+    users.push(req.body)
+
+    res.render('allUsers', { users })
+})
+
+app.post('/deleteuser/:id', (req,res) => {
+    console.log(req.params);
+    const { id } = req.params;
+    users.splice(id, 1)
+    res.render('allUsers', { users })
+    
+})
+
+app.get('/edituser/:id', (req,res) => {
+    const { id } = req.params;
+    res.render('editUser', { user: users[id], id })
+})
+
+app.post('/edituser/:id', (req,res) => {
+    const { id } = req.params;
+    const { fullname, email, course, age } = req.body;
+    users[id] = { fullname, email, course, age }
+    res.redirect('/users')
+})
+
+app.get('/addDbUser', (req,res) => {
+    let message = " "
+    res.render('addDbUser', { message })
+})
+
+app.post('/addDbUser', async(req,res) => {
+    const { fullname, email, password, age } = req.body;
+
+    let message;
+    try {
+        await UserModel.create(req.body)
+        res.send("User added successfully")
+        message = "User added successfully"
+        res.render('addDbUser', { message })
+    } catch (error) {
+        console.log(error);
+        message = "Error adding user"
+        res.render('addDbUser', { message })
     }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+}).on('error', (error) => {
+    console.log(`Error occurred while starting the server: ${error}`);
+});
